75a32b0eff5a5e707399a35f4f7e895975a744a7
Khizar Anjum <khizaranjum28@gmail.com>
2020-12-18 17:37:23 -0500

    added basic graph structure

--- graph.h
+++ graph.h
@@ -8,9 +8,25 @@ public:
 	int num_edges; //number of edges
 
 	// Methods of the class
-	graph();
+	graph(int num_vertices, int num_edges);
 	~graph();
 	int read(char* filename);
 	int* get_connected_elements(int vertex);
 	int* get_connected_weights(int vertex);
+};
+
+graph::graph(int num_vertices_, int num_edges_){
+	num_vertices = num_vertices_;
+	num_edges = num_edges_;
+	index_arr = new int[num_vertices];
+	num_connected = new int[num_vertices];
+	weight_arr = new int[num_edges];
+	connected_to = new int[num_edges];
+}
+
+graph::~graph(){
+	delete [] index_arr;
+	delete [] num_connected;
+	delete [] weight_arr;
+	delete [] connected_to;
 }
